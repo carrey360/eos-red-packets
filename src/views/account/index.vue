ba55b1cee2f8b236f167a81266293d98e6043b50
Leo <ly.liyang521@gmail.com>
2018-11-13 00:44:42 +0800

    remove format

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -51,123 +51,119 @@
 </template>
 
 <script>
-import topBar from "@/components/topBar";
-import modal from "@/components/dialog";
-import loading from "@/components/loading";
-import ecc from "eosjs-ecc";
-import LimitInput from "@/components/LimitInput";
-import { formatePacket, copy } from "@/utils/";
-import Eos from "eosjs";
+import topBar from '@/components/topBar'
+import modal from '@/components/dialog'
+import loading from '@/components/loading'
+import ecc from 'eosjs-ecc'
+import LimitInput from '@/components/LimitInput'
+import { formatePacket, copy } from '@/utils/'
+import Eos from 'eosjs'
 
 export default {
-  name: "account",
+  name: 'account',
   components: { topBar, modal, loading, LimitInput },
-  data() {
+  data () {
     return {
       showHome: true,
       hasRedCreateSuc: false,
       modalData: {
-        showDailog: false,
-        title: this.$t("提示"),
-        content: "",
-        type: "confirm",
-        btn: [{ text: this.$t("否") }, { text: this.$t("是") }]
+        'showDailog': false,
+        'title': this.$t('提示'),
+        'content': '',
+        'type': 'confirm',
+        'btn': [{text: this.$t('否')}, {text: this.$t('是')}]
       },
       packetNumber: this.$store.state.code,
       showLoading: false,
       userInput: {
-        accountName: "",
-        publicKey: "",
-        privateKey: ""
+        accountName: '',
+        publicKey: '',
+        privateKey: ''
       },
       formatCodeObj: {}
-    };
+    }
   },
   methods: {
-    leftBtnAction() {
-      this.modalData.showDailog = false;
+    leftBtnAction () {
+      this.modalData.showDailog = false
     },
-    rightBtnAction() {
+    rightBtnAction () {
       if (this.hasRedCreateSuc) {
-        this.modalData.showDailog = false;
-        return false;
+        this.modalData.showDailog = false
+        return false
       }
 
-      let formatCode = this.formatCodeObj;
+      let formatCode = this.formatCodeObj
       if (this.packetNumber && formatCode.isMemo) {
-        this.packetCreate();
+        this.packetCreate()
       } else {
-        this.$router.push({ path: "acTransfer", query: this.userInput });
+        this.$router.push({path: 'acTransfer', query: this.userInput})
       }
     },
-    create() {
+    create () {
       if (!this.userInput.accountName) {
-        window.tip(this.$t("请输入您的账号"));
-        return false;
+        window.tip(this.$t('请输入您的账号'))
+        return false
       } else if (this.userInput.accountName.length !== 12) {
-        window.tip(this.$t("请输入12位有效账号"));
-        return false;
+        window.tip(this.$t('请输入12位有效账号'))
+        return false
       } else if (!this.userInput.publicKey) {
-        window.tip(this.$t("请输入公钥"));
-        return false;
+        window.tip(this.$t('请输入公钥'))
+        return false
       }
       // 通过红包串创建账号
       if (this.packetNumber) {
-        this.showLoading = true;
+        this.showLoading = true
         this.$nextTick(() => {
-          let formatCode = formatePacket(this.packetNumber);
-          this.showLoading = false;
+          let formatCode = formatePacket(this.packetNumber)
+          this.showLoading = false
           if (!formatCode.isMemo) {
-            window.tip(this.$t("请正确输入红包串"));
-            return false;
+            window.tip(this.$t('请正确输入红包串'))
+            return false
           } else if (formatCode.isMemo) {
-            this.formatCodeObj = formatCode;
-            this.packetCreate();
+            this.formatCodeObj = formatCode
+            this.packetCreate()
             // this.modalData.content = this.$t('确定要用该红包创建账号')
             // this.modalData.showDailog = true
             // this.modalData.type = 'confirm'
           }
-        });
+        })
       } else {
         if (this.userInput.privateKey) {
-          this.modalData.content = this.$t("确定私钥已保存安全位置");
-          this.modalData.showDailog = true;
-          this.modalData.type = "confirm";
+          this.modalData.content = this.$t('确定私钥已保存安全位置')
+          this.modalData.showDailog = true
+          this.modalData.type = 'confirm'
         } else {
-          this.$router.push({ path: "acTransfer", query: this.userInput });
+          this.$router.push({path: 'acTransfer', query: this.userInput})
         }
       }
     },
-    packetCreate() {
-      this.modalData.showDailog = false;
+    packetCreate () {
+      this.modalData.showDailog = false
       this.$nextTick(() => {
-        this.showLoading = true;
-        this.packetCreateAction()
-          .then(result => {
-            // 跳转
-            this.showLoading = false;
-            if (result && result.transaction_id) {
-              this.modalData.content = this.$t(
-                "创建账号成功，请将私钥保存到安全位置"
-              );
-              this.modalData.showDailog = true;
-              this.modalData.type = "sure";
-              this.hasRedCreateSuc = true;
-            } else {
-              window.tip(result.error);
-            }
-          })
-          .catch(res => {
-            this.showLoading = false;
-            // let err = JSON.parse(String(res))
-            // window.tip((err.error.details && err.error.details[0] && err.error.details[0].message) || this.$t('失败'))
-            window.tip(this.$t("创建账号失败，可能账号已存在或红包金额不足"));
-          });
-      });
+        this.showLoading = true
+        this.packetCreateAction().then(result => {
+          // 跳转
+          this.showLoading = false
+          if (result && result.transaction_id) {
+            this.modalData.content = this.$t('创建账号成功，请将私钥保存到安全位置')
+            this.modalData.showDailog = true
+            this.modalData.type = 'sure'
+            this.hasRedCreateSuc = true
+          } else {
+            window.tip(result.error)
+          }
+        }).catch((res) => {
+          this.showLoading = false
+          // let err = JSON.parse(String(res))
+          // window.tip((err.error.details && err.error.details[0] && err.error.details[0].message) || this.$t('失败'))
+          window.tip(this.$t('创建账号失败，可能账号已存在或红包金额不足'))
+        })
+      })
     },
-    packetCreateAction() {
-      let eos = Eos(this.$store.state.eosConfig);
-      let formatCode = this.formatCodeObj;
+    packetCreateAction () {
+      let eos = Eos(this.$store.state.eosConfig)
+      let formatCode = this.formatCodeObj
 
       let params = {
         account_to_create: this.userInput.accountName,
@@ -175,151 +171,109 @@ export default {
         active_key: this.userInput.publicKey,
         id: +formatCode.uuid,
         sig: formatCode.sign
-      };
+      }
 
       let result = eos.transaction({
-        actions: [
-          {
-            account: this.$store.state.tranAccountName,
-            name: "create",
-            authorization: [
-              {
-                actor: this.$store.state.defaultAccount,
-                permission: "redpacket"
-              }
-            ],
-            data: params
-          }
-        ]
-      });
-      return result;
+        actions: [{
+          account: this.$store.state.tranAccountName,
+          name: 'create',
+          authorization: [{actor: this.$store.state.defaultAccount, permission: 'redpacket'}],
+          data: params
+        }]
+      })
+      return result
     },
-    createKey() {
-      this.showLoading = true;
+    createKey () {
+      this.showLoading = true
       this.$nextTick(() => {
-        ecc
-          .randomKey()
-          .then(privateKey => {
-            this.showLoading = false;
-            this.userInput.privateKey = privateKey;
-            this.userInput.publicKey = ecc.privateToPublic(privateKey);
-          })
-          .catch(() => {
-            this.showLoading = false;
-          });
-      });
+        ecc.randomKey().then(privateKey => {
+          this.showLoading = false
+          this.userInput.privateKey = privateKey
+          this.userInput.publicKey = ecc.privateToPublic(privateKey)
+        }).catch(() => {
+          this.showLoading = false
+        })
+      })
     },
-    copy(className) {
-      copy(className, this);
-    }
-  }
-};
-</script>
-
-<style lang="stylus" scoped>
-.account-warrap {
-  .account-content {
-    padding: 0 rem(16);
-    padding-bottom: rem(24);
-
-    .title {
-      margin-top: rem(24);
-      display: flex;
-      justify-content: space-between;
-      font-size: 14px;
-      color: #5D4220;
-
-      .copy {
-        color: #288EFB;
-        font-size: 14px;
-      }
-    }
-
-    .common-input {
-      width: calc(100% - 20px);
-      border: 0 none;
-      padding: 10px;
-      margin-top: 4px;
-      word-break: break-all;
-      resize: none;
-      font-size: 14px;
-      font-weight: 300;
-
-      &:focus {
-        outline: none;
-      }
-
-      &::placeholder {
-        color: #C9C2B7;
-      }
-    }
-
-    .account-name {
-      height: rem(28);
-      line-height: rem(28);
-      background-color: #F8F8F8;
-    }
-
-    .input-tip {
-      color: #A69987;
-      font-size: 12px;
-      margin-top: 4px;
-      text-align: left;
-    }
-
-    .public-key {
-      height: rem(70);
-      background-color: #F1FDF5;
-    }
-
-    .private-key {
-      height: rem(70);
-      background-color: #FBF2F2;
-    }
-
-    .red {
-      color: #CE2344;
-    }
-
-    .packet-number {
-      width: calc(100% - 22px);
-      height: rem(158);
-      padding: rem(12);
-      background-color: #FFFCDD;
-    }
-
-    .account-tip {
-      margin-top: rem(24);
-      text-align: left;
-      color: #5D4220;
-      font-size: 12px;
-      line-height: rem(20);
-      font-weight: 400;
-
-      p:first-child {
-        font-weight: 500;
-        font-size: 14px;
-      }
-    }
-
-    .button {
-      margin: rem(24) auto;
-      margin-bottom: 0;
-      height: 50px;
-      line-height: 50px;
-      color: #5D4220;
-      background-color: #FCDBB2;
-      border-radius: 2px;
-      font-weight: 500;
-      width: 100%;
-      font-size: 16px;
-      text-align: center;
-
-      &.disabled {
-        color: #A69987;
-        background-color: #FDEDD8;
-      }
+    copy (className) {
+      copy(className, this)
     }
   }
 }
+</script>
+
+<style lang="stylus" scoped>
+.account-warrap
+  .account-content
+    padding 0 rem(16)
+    padding-bottom rem(24)
+    .title
+      margin-top rem(24)
+      display flex
+      justify-content space-between
+      font-size 14px
+      color #5D4220
+      .copy
+        color #288EFB
+        font-size 14px
+    .common-input
+      width calc(100% - 20px)
+      border 0 none
+      padding 10px
+      margin-top 4px
+      word-break break-all
+      resize none
+      font-size 14px
+      font-weight 300
+      &:focus
+        outline none
+      &::placeholder
+        color #C9C2B7
+    .account-name
+      height rem(28)
+      line-height rem(28)
+      background-color #F8F8F8
+    .input-tip
+      color #A69987
+      font-size 12px
+      margin-top 4px
+      text-align left
+    .public-key
+      height rem(70)
+      background-color #F1FDF5
+    .private-key
+      height rem(70)
+      background-color #FBF2F2
+    .red
+      color #CE2344
+    .packet-number
+      width calc(100% - 22px)
+      height rem(158)
+      padding rem(12)
+      background-color #FFFCDD
+    .account-tip
+      margin-top rem(24)
+      text-align left
+      color #5D4220
+      font-size 12px
+      line-height rem(20)
+      font-weight 400
+      p:first-child
+        font-weight 500
+        font-size 14px
+    .button
+      margin rem(24) auto
+      margin-bottom 0
+      height 50px
+      line-height 50px
+      color #5D4220
+      background-color #FCDBB2
+      border-radius 2px
+      font-weight 500
+      width 100%
+      font-size 16px
+      text-align center
+      &.disabled
+        color #A69987
+        background-color #FDEDD8
 </style>
