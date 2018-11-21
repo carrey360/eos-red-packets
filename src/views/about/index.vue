557ace33d8fe02361c633a1a6307d53bbe1b844a
queqi <queqi@huobi.com>
2018-11-21 14:08:53 +0800

    修改关于我们

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -4,7 +4,7 @@
     <div class="content">
       <div>{{$t('EOS红包是由火币矿池（www.huobipool.com）和麒麟社区开发的在线领取和派发EOS红包的平台')}}</div>
       <div>{{$t('用户可以凭口令领取红包，也可以派发普通红包和拼手气红包')}}</div>
-      <div>{{$t('目前支持EOS币种、中英文版本')}}</div>
+      <div>{{$t('目前支持EOS,EGT币种、中英文版本')}}</div>
     </div>
   </div>
 </template>
